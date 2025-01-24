83d1b96bd8f4d9fc8fa27ca7ce40a2130fa4535f
Hey-Thing <159765239+Hey-Thing@users.noreply.github.com>
2025-01-24 22:58:49 +0100

    Bubble floatdown

---
+++ GGJG2025/Assets/Hedin/Bubble/Scrips/BubbleBlower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleBlower : MonoBehaviour
+{
+    public Transform Blower;
+    public GameObject Bubble;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Bubble, Blower.position, Blower.rotation);
+        }
+    }
+}
