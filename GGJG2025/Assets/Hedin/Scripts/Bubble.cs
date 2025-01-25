857de5452fcf38bf1767215084acfeec871c82f9
Hey-Thing <159765239+Hey-Thing@users.noreply.github.com>
2025-01-25 11:36:38 +0100

    oke it works again.

---
+++ GGJG2025/Assets/Hedin/Scripts/Bubble.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    public float initialSpeed = 10f;
+    private float speed;
+    private Rigidbody2D rb;
+    private float elapsedTime;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = Random.Range(1f, 3f);
+        initialSpeed = Random.Range(10f, 35f);
+        speed = initialSpeed;
+    }
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        rb.velocity = transform.right * speed;
+        speed = initialSpeed * Mathf.Exp(-elapsedTime * 2f);
+    }
+}
